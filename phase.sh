f3e68383f2b198682f968ac387a5df312d92efdb
Victor <victor3@ualberta.ca>
2019-11-22 22:38:21 -0700

    Test script in attempt to clean up Nebye Phase 2 code

---
+++ phase.sh
@@ -0,0 +1,37 @@
+# Run...
+# > chmod u+x phase02.sh
+# > ./phase02.sh
+# Make sure to install: "sudo apt-get install db-util"
+
+# Remove Index database files if they are there
+rm data/indexes/te.idx
+rm data/indexes/em.idx
+rm data/indexes/da.idx
+rm data/indexes/re.idx
+rm data/indexes/test/*
+
+# # Create db files
+# db_create -T -t hash data.indexes/re.idx
+# db_load -c dupsort=1 -T -t btree data/indexes/te.idx
+# db_load -c dupsort=1 -T -t btree data/indexes/em.idx
+# db_load -c dupsort=1 -T -t btree data/indexes/da.idx
+
+# touch data/indexes/re.idx
+# touch data/indexes/te.idx
+# touch data/indexes/em.idx
+# touch data/indexes/da.idx
+
+for file_name in $(ls data/output/);
+  sort -u -o data/output/$ {file_name} data/output/$ {file_name}
+
+# Load each result from stdout into it's apporpriate index
+perl break.pl < data/input/recs.txt | db_load -T -t hash data/indexes/re.idx
+perl break.pl < data/input/terms.txt | db_load -T -c dupsort=1 -t btree data/indexes/te.idx
+perl break.pl < data/input/emails.txt | db_load -T -c dupsort=1 -t btree data/indexes/em.idx
+perl break.pl < data/input/dates.txt | db_load -T -c dupsort=1 -t btree data/indexes/da.idx
+
+# Test results of each index, outputted into the tests folder (remove when submitting)
+db_dump -p -f data/indexes/tests/re_res.txt data/indexes/re.idx
+db_dump -p -f data/indexes/tests/te_res.txt data/indexes/te.idx
+db_dump -p -f data/indexes/tests/em_res.txt data/indexes/em.idx
+db_dump -p -f data/indexes/tests/da_res.txt data/indexes/da.idx
