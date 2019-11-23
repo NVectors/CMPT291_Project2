831eb3b57582f62bcd5a5ff5d39dee42eab38bfd
Victor <victor3@ualberta.ca>
2019-11-22 23:52:54 -0700

    remove

--- phase02/phase02_3.sh
+++
@@ -1,5 +0,0 @@
-# Load each result from stdout into it's apporpriate index
-perl break.pl < data/input/1recs.txt | db_load -T -t hash data/output/re.idx
-perl break.pl < data/input/1terms.txt | db_load -T -c dupsort=1 -t btree data/output/te.idx
-perl break.pl < data/input/1emails.txt | db_load -T -c dupsort=1 -t btree data/output/em.idx
-perl break.pl < data/input/1dates.txt | db_load -T -c dupsort=1 -t btree data/output/da.idx
