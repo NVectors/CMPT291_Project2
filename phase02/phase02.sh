058e462c215471d1274bc582e3b3cf557ebaaca6
Victor <victor3@ualberta.ca>
2019-11-22 21:42:49 -0700

    Revert "Add files via upload"
    This reverts commit 7a826039cbaea894b96b4301c08408d4e3b54738.

--- phase02/phase02.sh
+++ phase02/phase02.sh
@@ -31,8 +31,8 @@ sort -u data/input/10-dates.txt > data/output/dates.txt
 
 
 # Load each result from stdout into it's apporpriate index
-perl break.pl < data/output/1recs.txt | db_load -T -t hash data/output/re.idx
-perl break.pl < data/output/1terms.txt | db_load -T -c dupsort=1 -t btree data/output/te.idx
-perl break.pl < data/output/1emails.txt | db_load -T -c dupsort=1 -t btree data/output/em.idx
-perl break.pl < data/output/1dates.txt | db_load -T -c dupsort=1 -t btree data/output/da.idx
+perl break.pl < data/output/recs.txt | db_load -T -t hash data/output/re.idx
+perl break.pl < data/output/terms.txt | db_load -T -c dupsort=1 -t btree data/output/te.idx
+perl break.pl < data/output/emails.txt | db_load -T -c dupsort=1 -t btree data/output/em.idx
+perl break.pl < data/output/dates.txt | db_load -T -c dupsort=1 -t btree data/output/da.idx
 
